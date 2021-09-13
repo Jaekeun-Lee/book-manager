13ea71ac387f968cd2d0ac113e4f21a0abdc2895
Jaekeun Lee <64590527+Jaekeun-Lee@users.noreply.github.com>
2021-09-13 22:16:57 +0900

    20210913 | style : 기본 데이터 이름 수정

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,5 +1,5 @@
 insert into user (`id`, `name`, `email`, `created_at`, `updated_at`) values (1, 'martin', 'martin@naver.com', now(), now());
 insert into user (`id`, `name`, `email`, `created_at`, `updated_at`) values (2, 'dennis', 'dennis@gmail.com', now(), now());
 insert into user (`id`, `name`, `email`, `created_at`, `updated_at`) values (3, 'sophia', 'sophia@naver.com', now(), now());
-insert into user (`id`, `name`, `email`, `created_at`, `updated_at`) values (4, 'james', 'james@gmail.com', now(), now());
+insert into user (`id`, `name`, `email`, `created_at`, `updated_at`) values (4, 'jack', 'james@gmail.com', now(), now());
 insert into user (`id`, `name`, `email`, `created_at`, `updated_at`) values (5, 'jack', 'jack@naver.com', now(), now());
